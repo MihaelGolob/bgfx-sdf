367cb1d72fb34fe58444b49836b4256d4a015bf8
mihael.golob <mihael.golob@outfit7.com>
2024-04-08 11:09:00 +0200

    Add initialization of freetype

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "bgfx/bgfx.h"
 #include "GLFW/glfw3.h"
 #include "GLFW/glfw3native.h"
+#include "ft2build.h"
+#include FT_FREETYPE_H
 
 #include <iostream>
 
@@ -12,6 +14,7 @@ constexpr int k_window_height_ = 900;
 
 // global variables
 GLFWwindow* window_;
+FT_Library  ft_;
 
 GLFWwindow* CreateAndLinkWindow() {
     glfwInit();
@@ -25,13 +28,28 @@ GLFWwindow* CreateAndLinkWindow() {
     bgfxInit.platformData.nwh = glfwGetWin32Window(window);
     bgfx::init(bgfxInit);
 
-    return window;
-}
-
-void Init() {
     bgfx::setDebug(BGFX_DEBUG_NONE);
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
     bgfx::setViewRect(0, 0, 0, k_window_width_, k_window_height_);
+
+    return window;
+}
+
+void InitFreetype() {
+    if (FT_Init_FreeType(&ft_)) {
+        std::cerr << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+    } 
+}
+
+FT_Face LoadFont(const char* font_path, int font_size) {
+    FT_Face face;
+    if (FT_New_Face(ft_, font_path, 0, &face)) {
+        std::cerr << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    }
+    if (FT_Set_Char_Size(face, 0, font_size * 64, k_window_width_, k_window_height_)) {
+        std::cerr << "ERROR::FREETYPE: Failed to set char size" << std::endl;
+    }
+    return face;
 }
 
 void Update() {
@@ -50,9 +68,14 @@ void Shutdown() {
 }
 
 int main() {
+    // initialization
     window_ = CreateAndLinkWindow();
-    Init();
+    InitFreetype();
+    const auto font_face = LoadFont(R"(..\assets\fonts\freedom.ttf)", 20);
+    
+    // update loop
     Update();
+    
     Shutdown();
     return 0;
 }
