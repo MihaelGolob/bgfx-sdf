2a254db1e3a6a551f543f0f662265e2253b9065d
mihael.golob <golob.mihael@gmail.com>
2024-07-05 15:13:28 +0200

    FIX: main bgfx-sdf project is now a library, created new project with main for running it. Unit testing also works as expected now.

--- cmake-build-release/libs/bgfx.cmake/cmake/bgfx/generated/shader.cpp
+++
@@ -1,3 +0,0 @@
-#include "C:/Users/Mihael/Documents/bgfx-sdf/libs/bgfx.cmake/bgfx/src/shader.cpp"
-#include "C:/Users/Mihael/Documents/bgfx-sdf/libs/bgfx.cmake/bgfx/src/shader_dxbc.cpp"
-#include "C:/Users/Mihael/Documents/bgfx-sdf/libs/bgfx.cmake/bgfx/src/shader_spirv.cpp"
--- cmake-build-release/libs/bgfx.cmake/cmake/bgfx/generated/vertexlayout.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Mihael/Documents/bgfx-sdf/libs/bgfx.cmake/bgfx/src/vertexlayout.cpp"
