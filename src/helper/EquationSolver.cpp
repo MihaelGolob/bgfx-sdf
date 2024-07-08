3f38651de64aec8c34b7f62a1b15af3a3731f52c
mihael.golob <mihael.golob@outfit7.com>
2024-07-08 10:47:06 +0200

    NEW: implement cubic equation solver

--- src/helper/EquationSolver.cpp
+++ src/helper/EquationSolver.cpp
@@ -4,9 +4,18 @@
 
 #include <vector>
 #include "EquationSolver.h"
+#include <poly34.h>
+#include <algorithm>
 
 std::vector<double> SolveCubicEquation(double a, double b, double c, double d) {
-    std::vector<double> roots;
+    double r[3];
+    int n = SolveP3(r, b/a, c/a, d/a);
+    
+    auto roots = std::vector<double>(n);
+    for (int i = 0; i < n; i++) {
+        roots.emplace_back(r[i]);
+    }
+    std::sort(roots.begin(), roots.end());
 
     return roots;
 }
