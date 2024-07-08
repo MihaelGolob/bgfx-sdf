435f2fc972163e379ee484af1a949358192e4708
mihael.golob <mihael.golob@outfit7.com>
2024-07-08 14:44:35 +0200

    NEW: implement shape distance and signed distance

--- src/font_decomposition/Shape.cpp
+++ src/font_decomposition/Shape.cpp
@@ -2,6 +2,7 @@
 // Created by MihaelGolob on 3. 07. 2024.
 //
 
+#include <cmath>
 #include "Shape.h"
 
 Contour& Shape::AddEmptyContour() {
@@ -21,4 +22,46 @@ void Shape::ApplyEdgeColoring(float max_angle) {
             else current_color = EdgeColor::Yellow;
         }
     }
+}
+
+double Shape::Distance(const Vector2 &p) const {
+    double min_distance = INFINITY;
+    
+    for (const auto& c : contours) {
+        for (const auto& e : c.edges) {
+            double t;
+            auto distance = e->Distance(p, t);
+            if (distance < min_distance) {
+                min_distance = distance;
+            }
+        }
+    }
+    
+    return min_distance;
+}
+
+double Shape::SignedDistance(const Vector2 &p) const {
+    double min_distance = INFINITY;
+    double max_orthogonality = 0;
+    
+    for (const auto& c : contours) {
+        for (const auto& e : c.edges) {
+            double t;
+            auto distance = e->SignedDistance(p, t);
+            
+            if (distance < min_distance) {
+                min_distance = distance;
+                max_orthogonality = e->GetOrthogonality(p, t);
+            } else if (distance == min_distance) {
+                const auto orthogonality = e->GetOrthogonality(p, t);
+                if (orthogonality > max_orthogonality) {
+                    // this is needed for correct sign if the distance is the same
+                    max_orthogonality = orthogonality;
+                    min_distance = distance;
+                }
+            }
+        }
+    }
+    
+    return min_distance;
 }
--- src/font_decomposition/Shape.h
+++ src/font_decomposition/Shape.h
@@ -11,6 +11,8 @@ public:
     Contour& AddEmptyContour();
     
     void ApplyEdgeColoring(float max_angle);
+    [[nodiscard]] double Distance(const Vector2 &p) const;
+    [[nodiscard]] double SignedDistance(const Vector2 &p) const;
     
 public:
     std::vector<Contour> contours;
