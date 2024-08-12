9ac6b6183c3692cfc4d05f90cd9cf954ceae4ba6
mihael.golob <mihael.golob@outfit7.com>
2024-08-12 10:51:01 +0200

    NEW: Add Timer class for measuring scope execution time

---
+++ benchmark/Timer.cpp
@@ -0,0 +1,27 @@
+﻿//
+// Created by MihaelGolob on 12. 08. 2024.
+//
+
+#include "Timer.h"
+#include <iostream>
+
+Timer::Timer(const std::string &name) {
+    start_timepoint_ = std::chrono::high_resolution_clock::now();
+    name_ = name;
+}
+
+Timer::~Timer() {
+    Stop();
+}
+
+void Timer::Stop() {
+    auto end_timepoint = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::time_point_cast<std::chrono::microseconds>(start_timepoint_).time_since_epoch().count();
+    auto end_time = std::chrono::time_point_cast<std::chrono::microseconds>(end_timepoint).time_since_epoch().count();
+    
+    auto duration = end_time - start_time;
+    double ms = duration * 0.001;
+    
+    std::cout << "\033[1;36mTimer-" << name_ << ": " << duration << "us (" << ms << "ms)" << "\033[0m" << std::endl;
+}
+
---
+++ benchmark/Timer.h
@@ -0,0 +1,18 @@
+﻿//
+// Created by MihaelGolob on 12. 08. 2024.
+//
+
+#include <chrono>
+#include <string>
+
+class Timer {
+public:
+    explicit Timer(const std::string& name);
+    ~Timer();
+    
+private:
+    void Stop();
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_timepoint_;
+    std::string name_;
+};
