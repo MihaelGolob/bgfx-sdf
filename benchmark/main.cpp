2ae67bf8766cf10836317d6f936f024fe67ad297
mihael.golob <mihael.golob@outfit7.com>
2024-08-13 11:49:20 +0200

    REFAC: autoformatting

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -9,9 +9,9 @@
 const int k_window_width_ = 1500;
 const int k_window_height_ = 900;
 
-Window* window_;
-FontManager* font_manager_;
-TextBufferManager* text_buffer_manager_;
+Window *window_;
+FontManager *font_manager_;
+TextBufferManager *text_buffer_manager_;
 TrueTypeHandle font_file_;
 
 auto font_types_to_test_ = std::vector<FontType>{FontType::Bitmap, FontType::SdfFromBitmap, FontType::SdfFromVector, FontType::Msdf};
@@ -24,13 +24,13 @@ void Setup() {
 
 void Shutdown() {
     font_manager_->DestroyTtf(font_file_);
-    
+
     delete font_manager_;
     delete text_buffer_manager_;
     delete window_;
 }
 
-void PrintGlyphBenchmarkResults(const std::string& name, std::vector<double> results, std::vector<FontType> font_types) {
+void PrintGlyphBenchmarkResults(const std::string &name, std::vector<double> results, std::vector<FontType> font_types) {
     PrintGreen(name.c_str());
     for (int i = 0; i < font_types.size(); i++) {
         std::string str = "Font type ";
@@ -40,21 +40,20 @@ void PrintGlyphBenchmarkResults(const std::string& name, std::vector<double> res
         str.append(" ms.");
 
         PrintGreen(str.c_str());
-    } 
+    }
 }
 
 void BenchmarkGlyphGeneration() {
-    
     auto glyph_generation_benchmark = GlyphGenerationBenchmark(font_manager_, font_file_);
     glyph_generation_benchmark.SetupBenchmark(font_types_to_test_, 500, false);
     auto mean_times = glyph_generation_benchmark.RunBenchmark();
-    
+
     PrintGlyphBenchmarkResults("Glyph Generation Results (ms / glyph generated)", mean_times, font_types_to_test_);
 }
 
 void BenchmarkGlyphRendering() {
     auto glyph_rendering_benchmark = GlyphRenderingBenchmark(window_, font_manager_, text_buffer_manager_, font_file_);
-    glyph_rendering_benchmark.SetupBenchmark(500, 60, font_types_to_test_, [&](const std::vector<double>& results) {
+    glyph_rendering_benchmark.SetupBenchmark(500, 60, font_types_to_test_, [&](const std::vector<double> &results) {
         PrintGlyphBenchmarkResults("Glyph Rendering Results (ms / frame)", results, font_types_to_test_);
     });
     glyph_rendering_benchmark.RunBenchmark();
@@ -66,7 +65,7 @@ int main() {
 
     BenchmarkGlyphGeneration();
     BenchmarkGlyphRendering();
-    
+
     Shutdown();
     return 0;
 }
