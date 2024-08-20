4bbf696e8aded67a2704aec0f57b333877e82bda
mihael.golob <mihael.golob@outfit7.com>
2024-08-20 12:44:04 +0200

    NEW: buggy original msdf works

--- src/sdf/MsdfOriginalGenerator.cpp
+++ src/sdf/MsdfOriginalGenerator.cpp
@@ -21,26 +21,36 @@ void MsdfOriginalGenerator::BakeGlyphMsdf(CodePoint code_point, GlyphInfo &glyph
         PrintError("Failed to load glyph");
         return;
     }
+    
+    msdfgen::FontMetrics metrics{};
+    msdfgen::getFontMetrics(metrics, font_);
+    
+    const auto bounds = shape.getBounds();
+
+    // TODO: temp
+    glyph_info.width = font_size_;
+    glyph_info.height = font_size_;
+    glyph_info.advance_x = 20;
+    glyph_info.advance_y = 0;
+    glyph_info.offset_x = bounds.l;
+    glyph_info.offset_y = -bounds.b;
 
     shape.normalize();
     msdfgen::edgeColoringSimple(shape, 3.0);
     msdfgen::Bitmap<float, 3> msdf(font_size_, font_size_);
-    msdfgen::SDFTransformation t(msdfgen::Projection(font_size_, msdfgen::Vector2(0.0, 0.0)), msdfgen::Range(255.0));
+    msdfgen::SDFTransformation t(msdfgen::Projection(font_size_, msdfgen::Vector2(0.0, 0.0)), msdfgen::Range(1.0));
     msdfgen::generateMSDF(msdf, shape, t);
 
     for (int y = 0; y < msdf.height(); y++) {
         for (int x = 0; x < msdf.width(); x++) {
-            const auto pixel = msdf(x, y);
+            const auto pixel = msdf(x, msdf.width() - y);
             const auto index = (y * msdf.width() + x) * 4;
             
-            printf("(%f, %f, %f)\n", pixel[0], pixel[1], pixel[2]);
-
             output[index + 0] = static_cast<uint8_t>(pixel[2] * 255);
             output[index + 1] = static_cast<uint8_t>(pixel[1] * 255);
             output[index + 2] = static_cast<uint8_t>(pixel[0] * 255);
             output[index + 3] = 255;
         }
-        printf("\n");
     }
 }
 
