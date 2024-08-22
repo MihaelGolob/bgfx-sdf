2341c27e5a49160957b123e52bbec3a149f70d24
mihael.golob <mihael.golob@outfit7.com>
2024-08-22 12:33:24 +0200

    NEW: create structure for glyph precision benchmarking

---
+++ benchmark/GlyphPrecisionBenchmark.cpp
@@ -0,0 +1,19 @@
+﻿//
+// Created by MihaelGolob on 22. 08. 2024.
+//
+
+#include <utilities.h>
+#include "GlyphPrecisionBenchmark.h"
+
+GlyphPrecisionBenchmark::GlyphPrecisionBenchmark(FontManager *font_manager, TrueTypeHandle font_handle) :
+        font_manager_(font_manager), font_handle_(font_handle) {
+    render_texture_ = bgfx::createTexture2D(1024, 1024, false, 1, bgfx::TextureFormat::RGBA8, BGFX_TEXTURE_READ_BACK);
+    if (!bgfx::isValid(render_texture_)) PrintError("Failed to create render texture");
+    
+}
+
+GlyphPrecisionBenchmark::~GlyphPrecisionBenchmark() {
+    bgfx::destroy(render_texture_);
+}
+
+
---
+++ benchmark/GlyphPrecisionBenchmark.h
@@ -0,0 +1,21 @@
+﻿//
+// Created by MihaelGolob on 22. 08. 2024.
+//
+
+#pragma once
+
+#include <font_processing/FontHandles.h>
+
+class FontManager;
+
+class GlyphPrecisionBenchmark {
+public:
+    GlyphPrecisionBenchmark(FontManager* font_manager, TrueTypeHandle font_handle);
+    ~GlyphPrecisionBenchmark();
+    
+private:
+    FontManager* font_manager_;
+    TrueTypeHandle font_handle_{};
+    
+    bgfx::TextureHandle render_texture_{};
+};
